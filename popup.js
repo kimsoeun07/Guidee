43e8738c7a3ab52b2fd250c786e0dfa55e064ac2
quddkflQkd <165963943+quddkflQkd@users.noreply.github.com>
2025-07-15 22:45:46 +0900

    really last fixing..

--- popup.js
+++ popup.js
@@ -1,1 +1,18 @@
 console.log("Popup script loaded!");
+
+document.getElementById("runCrawler").addEventListener("click", async () => {
+  console.log("🧠 버튼 클릭됨! content script에 HTML 요청 보냄!!");
+
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.tabs.sendMessage(tab.id, { type: "CRAWL_HTML" }, (response) => {
+    if (chrome.runtime.lastError) {
+      console.error(
+        "❌ content script 응답 실패:",
+        chrome.runtime.lastError.message
+      );
+    } else {
+      console.log("✅ 응답 받음:", response);
+    }
+  });
+});
